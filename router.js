5b358af9a1ace17c102f16b981bf0e58b0efd63f
Bolin Chan <bolinchan2046@gmail.com>
2020-03-19 14:22:28 +0800

    update

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ const pages = {
 }
 
 const toNav = (url, query = {}, type = "navigateTo") => {
-	if (url.indexOf("/") === -1) url = url in pages ? pages[url] : "";
+	if (url.indexOf("/") === -1) url = url in pages? pages[url]: "";
 	if (!url) return toast("页面错误或不存在");
 	const queryStr = encodeURIComponent(JSON.stringify(query));
 	uni[type]({ url: `/${url}?query=${queryStr}` });

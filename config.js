2b794fed2949957f8d8865f05c9d40cbf2b19c8b
Bolin Chan <bolinchan2046@gmail.com>
2020-03-19 14:13:14 +0800

    update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export default {
 	// api地址
-	api_url: 'http://labapinfang.jlshybx.com',
+	api_url: "http://labapinfang.jlshybx.com",
 	// 上传地址
-	upload_url: 'http://labapinfang.jlshybx.com/api/uploader/uploadimg',
+	upload_url: "http://labapinfang.jlshybx.com/api/uploader/uploadimg",
 	// 更新地址
-	wgt_url: 'http://www.xi0013.com/?s=/api/update'
+	wgt_url: "http://www.xi0013.com/?s=/api/update"
 }
--- router.js
+++ router.js
@@ -1,19 +1,14 @@
-import { toast } from "./common/tools";
+import { toast } from "common/tools";
 
 const pages = {
 	home: "pages/home/home"
 }
 
 const toNav = (url, query = {}, type = "navigateTo") => {
-	if (url.indexOf("/") === -1) {
-		url = url in pages ? pages[url] : "";
-	}
-	if (url) {
-		const queryStr = encodeURIComponent(JSON.stringify(query));
-		uni[type]({ url: `/${url}?query=${queryStr}` });
-	} else {
-		toast("页面错误或不存在");
-	}
+	if (url.indexOf("/") === -1) url = url in pages ? pages[url] : "";
+	if (!url) return toast("页面错误或不存在");
+	const queryStr = encodeURIComponent(JSON.stringify(query));
+	uni[type]({ url: `/${url}?query=${queryStr}` });
 }
 
 export default toNav;
